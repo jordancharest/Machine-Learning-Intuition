e8a99f1a4aff54cd0fb43a639a29b4707993a4bb
Jordan <jordancharest3@gmail.com>
2017-12-29 15:05:58 -0500

    Rename Folder

--- Data Preprocessing/data_preprocessing.py
+++ Data Preprocessing/data_preprocessing.py
@@ -34,6 +34,5 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
-# not necessarily required since y already ranges from 0 to 1
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)
