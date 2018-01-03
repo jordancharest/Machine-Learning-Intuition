b8835eb7836e7a522a6f07485daf584631bdc1a0
Jordan <jordancharest3@gmail.com>
2018-01-03 13:52:51 -0500

    Finished K-means Clustering

--- Clustering/k_means.py
+++ Clustering/k_means.py
@@ -1,45 +1,35 @@
 # K-Means Clustering
 
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+# Import the dataset
 dataset = pd.read_csv('Mall_Customers.csv')
 X = dataset.iloc[:, [3, 4]].values
-# y = dataset.iloc[:, 3].values
 
-# Splitting the dataset into the Training set and Test set
-"""from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
-
-# Using the elbow method to find the optimal number of clusters
+# Use the elbow method to find the optimal number of clusters
 from sklearn.cluster import KMeans
 wcss = []
 for i in range(1, 11):
-    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+    kmeans = KMeans(n_clusters = i, init = 'k-means++')
     kmeans.fit(X)
     wcss.append(kmeans.inertia_)
+    
+f = plt.figure(1)
 plt.plot(range(1, 11), wcss)
 plt.title('The Elbow Method')
 plt.xlabel('Number of clusters')
 plt.ylabel('WCSS')
-plt.show()
+f.show()
 
-# Fitting K-Means to the dataset
-kmeans = KMeans(n_clusters = 5, init = 'k-means++', random_state = 42)
+# Fit K-Means to the dataset
+kmeans = KMeans(n_clusters = 5, init = 'k-means++')
 y_kmeans = kmeans.fit_predict(X)
 
-# Visualising the clusters
+# Visualize the clusters
+g = plt.figure(2)
 plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
 plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
 plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
@@ -50,4 +40,4 @@ plt.title('Clusters of customers')
 plt.xlabel('Annual Income (k$)')
 plt.ylabel('Spending Score (1-100)')
 plt.legend()
-plt.show()
+g.show()
